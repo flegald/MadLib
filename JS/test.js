dd2211199bc0389ac2c23589a63bab79be56ca9a
DonChatelain <djchatelain@gmail.com>
2015-11-07 14:51:06 -0800

    adds style and organizes code

--- JS/test.js
+++ JS/test.js
@@ -1,14 +1,25 @@
 $(document).ready(function(){
-	$('.nounDrag').draggable({snap: '.nounTar'});
-   $('.nounTar').droppable({accept: '.nounDrag',
-            drop: function( event, ui ) {
-               $(this)
-               .addClass( "dropped" )
-            $(document).ready(function(){
-            });
-         }
-
+  $('.nounDrag').draggable({snap: '.nounTar, .verbTar', snapMode: 'inner'});
+  $('.verbDrag').draggable({snap: '.nounTar, .verbTar', snapMode: 'inner'});
+  $('.nounTar').droppable({
+    drop: function(event, ui) {
+      if ((ui.draggable).hasClass('nounDrag')) {
+        console.log('yes')
+      } else {
+        console.log('no')
+      }
+    }
+  });
+  $('.verbTar').droppable({
+    drop: function(event, ui) {
+      if ((ui.draggable).hasClass('verbDrag')) {
+        console.log('yes')
+      } else {
+        console.log('no')
+      }
+    }
+  });
 });
-});   
 
-	
+
+
